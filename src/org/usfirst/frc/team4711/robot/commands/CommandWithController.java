cb52b3115ee05c3c3ae3d69f1664829c37671337
James Hochadel <james.hochadel@gmail.com>
2018-03-07 19:55:11 -0800

    Simplified drive-with-trigger logic

--- src/org/usfirst/frc/team4711/robot/commands/CommandWithController.java
+++ src/org/usfirst/frc/team4711/robot/commands/CommandWithController.java
@@ -1,9 +1,9 @@
 package org.usfirst.frc.team4711.robot.commands;
 
-import org.usfirst.frc.team4711.robot.config.RobotMap;
-import org.usfirst.frc.team4711.robot.subsystems.ControllerSubsystem;
 import org.usfirst.frc.team4711.robot.Utils;
 import org.usfirst.frc.team4711.robot.config.KeyMap;
+import org.usfirst.frc.team4711.robot.config.RobotMap;
+import org.usfirst.frc.team4711.robot.subsystems.ControllerSubsystem;
 import org.usfirst.frc.team4711.robot.subsystems.DriveTrain;
 import org.usfirst.frc.team4711.robot.subsystems.ElevatorSubsystem;
 
@@ -11,58 +11,60 @@ import org.usfirst.frc.team4711.robot.subsystems.ElevatorSubsystem;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class CommandWithController extends Command {
-	
+
 	private ControllerSubsystem _controller;
 	private DriveTrain _drive;
-	private ElevatorSubsystem _elevator; 
-	
+	private ElevatorSubsystem _elevator;
+
 	public CommandWithController() {
 		super("Drive With Joystick");
-		
+
 		_controller = ControllerSubsystem.getInstance();
 		requires(_controller);
-		
+
 		_drive = DriveTrain.getInstance();
 		requires(_drive);
-		
+
 		_elevator = ElevatorSubsystem.getInstance();
 		requires(_elevator);
 	}
-	
+
+	@Override
 	protected void initialize() {
-		
+
     }
 
-    protected void execute() {
-    	
-    	_drive.arcadeDrive((_controller.getController().getRawAxis(KeyMap.ACCEL_FORWARD) > 0)? 
-    											_controller.getController().getRawAxis(KeyMap.ACCEL_FORWARD) :
-    											((_controller.getController().getRawAxis(KeyMap.ACCEL_BACK) > 0)? 
-    													-_controller.getController().getRawAxis(KeyMap.ACCEL_BACK):
-    														0.0), _controller.getController().getRawAxis(RobotMap.AXIS_LEFT_X));
+	/**
+	 * Drive arcadeDrive with the controller.
+	 */
+    @Override
+	protected void execute() {
+    	double driveSpeed = _controller.getController().getRawAxis(KeyMap.ACCEL_FORWARD) +
+    			_controller.getController().getRawAxis(KeyMap.ACCEL_BACK);
+		double driveAngle = _controller.getController().getRawAxis(RobotMap.AXIS_LEFT_X);
 
-    	_elevator.setMotorSpeed(_controller.getController().getRawAxis(RobotMap.AXIS_RIGHT_Y));
-    	
+    	_drive.arcadeDrive(driveSpeed, driveAngle);
+
     	//testing logging
-    	System.out.println("left : " + _drive.getCurrentLeftPosition() + 
+    	System.out.println("left : " + _drive.getCurrentLeftPosition() +
     						", right : " + _drive.getCurrentRightPosition() +
     						", elevator : " + Utils.convertPositionToInches(_elevator.getPosition(), RobotMap.ELEVATOR_WHEEL_DIAMETER));
     }
-    
+
 	@Override
 	protected boolean isFinished() {
 		return false;
 	}
-	
+
 	@Override
     protected void end() {
 		_drive.stop();
 		_elevator.setMotorSpeed(0.0);
     }
-	
+
 	@Override
     protected void interrupted() {
         end();
     }
-	
+
 }
